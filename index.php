b9aa08076f5ba5f659517d78f610860201a7c3fe
Gregory <goutgregoryy@gmail.com>
2020-03-30 10:34:02 +0200

    modif4

--- index.php
+++ index.php
@@ -8,31 +8,41 @@ class Personnage {
     public $etat;
 
     public function caracteristiques() {
+        
         echo "Le personnage" . " ". $this->nom ." ". "a une force de " ." ". $this->force . "."
-        ."Il est de niveau"." ". $this->level ." ". "et il a une santé de " ." ". $this->health ."."." ". "Il est" ." ". $this->etat . ".";
+        ."Il est de niveau"." ". $this->level ." ". "et il a une santé de " ." ". $this->health ."."." ". "Il est" ." ". $this->etat() . ".";
     }
-};
+
+    public function etat() {
+        if ($this->health >=1) {
+            $etat = "vivant";
+        }else {
+            $etat = "mort";
+        }
+        return $etat;
+}
+    };
 
 $perso1 = new Personnage();
 $perso1->nom ="Rose";
 $perso1->force = 12;
 $perso1->level = 1;
 $perso1->health = 100;
-$perso1->etat = true;
+$perso1->etat = "";
 $perso2 = new Personnage();
 $perso2->nom ="Jules";
 $perso2->force = 15;
 $perso2->level = 1;
 $perso2->health = 50;
-$perso2->etat = true;
+$perso2->etat = "";
 $perso3 = new Personnage();
 $perso3->nom ="Jean";
 $perso3->force = 6;
 $perso3->level = 1;
 $perso3->health = 0;
-$perso3->etat = false;
+$perso3->etat = "";
 
-$etat = isset($_POST['etat'])? "Vivant(e)" : "Mort" ;
+var_dump($perso1);
 
 $perso1-> caracteristiques();
 
